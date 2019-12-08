343442168bf03f32a14726ad2d945cb370025dee
Lea Linin <lea.linin@gmail.com>
2019-12-08 03:33:29 +0000

    Passed in an IP address to test

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+fetchCoordsByIP('24.86.85.102', (error, coords) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
-
-  console.log('It worked! Returned IP:', ip);
+  
+  console.log('It worked! Returned Coords:', coords);
 });
