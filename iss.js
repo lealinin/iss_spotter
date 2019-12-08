02fb91cc90a031fc32daed5eb8cb0e1f06b2288c
Lea Linin <lea.linin@gmail.com>
2019-12-08 03:31:38 +0000

    Added fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -12,23 +12,41 @@
 
 const request = require('request');
 
-const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+// const fetchMyIP = function(callback) {
+//   // use request to fetch IP address from JSON API
+//   request('https://api.ipify.org/?format=json', (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
 
-    if (response.statusCode !== 200) {
-      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+//     if (response.statusCode !== 200) {
+//       const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+  if (error) {
+    callback(error, null);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+  const { latitude, longitude } = JSON.parse(body).data;
+  callback(null, { latitude, longitude });
   });
-};
+}
 
-module.exports = { fetchMyIP };
+// module.exports = { fetchMyIP };
+
+module.exports = { fetchCoordsByIP };
