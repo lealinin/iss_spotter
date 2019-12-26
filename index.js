b74b849adc14d69f6f72f66ba9e3953422f4bf47
Lea Linin <lea.linin@gmail.com>
2019-12-26 06:24:05 +0000

    Added fetchISSFlyOverTimes test function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,11 +11,22 @@ const { fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
-fetchCoordsByIP('24.86.85.102', (error, coords) => {
+// fetchCoordsByIP('24.86.85.102', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+  
+//   console.log('It worked! Returned Coords:', coords);
+// });
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
-  
-  console.log('It worked! Returned Coords:', coords);
+
+  console.log('It worked! Returned flyover times:', passTimes);
 });
